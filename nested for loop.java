2fd94b773922297b2a12b5e56f4910984d03ca19
Sidhartt1 <109621884+Sidhartt1@users.noreply.github.com>
2022-07-21 01:15:05 +0530

    Update nested for loop.java

--- nested for loop.java
+++ nested for loop.java
@@ -4,9 +4,9 @@ public class world
 {
 	public static void main(String args[])
 	{
-		for(int a = 1; a<=3 ; a++)
+		for(int a=1; a<=3 ; a++)
 		{
-			for(int b =1; b<=3; b++)
+			for(int b=1; b<=3; b++)
 			{
 				if(a==2 && b==2)
 				{
