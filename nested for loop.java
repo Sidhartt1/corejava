53f39dacbdd6ea72169a21bc2722defe281ad3ba
Sidhartt1 <sidharttansankar1905@gmail.com>
2022-07-21 01:14:08 +0530

    Nestedforloop

---
+++ nested for loop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class world
+{
+	public static void main(String args[])
+	{
+		for(int a = 1; a<=3 ; a++)
+		{
+			for(int b =1; b<=3; b++)
+			{
+				if(a==2 && b==2)
+				{
+					break;
+				}
+				System.out.println(a+" "+b);
+			}
+		}
+	}
+	
+}
